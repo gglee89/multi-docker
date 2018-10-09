6a8bc4f326ce496519fc1362f7971112590fd8cd
gglee89 <gglee89@gmail.com>
2018-10-09 21:50:56 +0900

    circleci: Add DigitalOcean executable script deploy_app.

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,1 +1,19 @@
-echo "HELLO_WORLD!!!!!!!!"
+#!/bin/bash
+########################################
+# Put this on a Server
+# run chmod +x deploy_app.sh to make the script executable
+#
+# Execute this script:  ./deploy_app.sh
+#
+########################################
+
+set -e
+
+echo "Deploying multi-docker"
+
+docker run -d gustalee/multi-client
+docker run -d --rm=true -p 80:8000 gustavolee/multi-nginx
+docker run -d gustalee/multi-server
+docker run -d gustalee/multi-worker
+
+docker ps -a
